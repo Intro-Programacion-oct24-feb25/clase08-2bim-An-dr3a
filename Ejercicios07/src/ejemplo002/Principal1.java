1cc9cc124a9694bf640390a8fa135c9a1123547a
An-dr3a <criolloandrea20@hotmail.com>
2025-02-05 20:57:47 -0500

    Trabajo

--- Ejercicios07/src/ejemplo002/Principal1.java
+++ Ejercicios07/src/ejemplo002/Principal1.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ejemplo002;
+
 /**
  *
  * @author reroes
@@ -10,51 +11,86 @@ package ejemplo002;
 public class Principal1 {
 
     public static void main(String[] args) {
-        
-        String [] nombres = { "Jason", "Jonathan", "Kristen", "Robin", "Michelle", "Emily", "Noah", "Daniel"};
-        String [] apellidos = { "Lynch", "George", "Lang", "Cochran", "Young", "Fletcher", "Adkins", "Harris"};
-        int [][] notas = { {10, 80, 80, 95}, {40, 80, 80, 45}, {80, 10, 20, 55}, {70, 30, 20, 65}, 
-            {60, 50, 70, 75}, {50, 70, 30, 85},{40, 80, 40, 45}, {30, 90, 50, 95}};
-        
+
+        String[] nombres = {"Jason", "Jonathan", "Kristen", "Robin", "Michelle", "Emily", "Noah", "Daniel"};
+        String[] apellidos = {"Lynch", "George", "Lang", "Cochran", "Young", "Fletcher", "Adkins", "Harris"};
+        int[][] notas = {{10, 80, 80, 95}, {40, 80, 80, 45}, {80, 10, 20, 55}, {70, 30, 20, 65},
+        {60, 50, 70, 75}, {50, 70, 30, 85}, {40, 80, 40, 45}, {30, 90, 50, 95}};
+
         double promedio_paralelo = obtenerPromedioParalelo(notas);
         String nombre;
         String apellido;
         String tipoNotas;
         double promedioEstudiante;
         int numeroNotasArribaPromedio;
-        int [] filaNotas;
+        int[] filaNotas;
         String mensajeFinal = "";
         for (int i = 0; i < nombres.length; i++) {
             nombre = nombres[i];
             apellido = apellidos[i];
             filaNotas = notas[i];
+            String username = funcionUser(nombre, apellido);
+
             promedioEstudiante = funcion01(filaNotas);
-            numeroNotasArribaPromedio = funcion02(filaNotas, 
+            numeroNotasArribaPromedio = funcion02(filaNotas,
                     promedio_paralelo);
             tipoNotas = funcion03(filaNotas);
-            mensajeFinal = String.format("%s%s\n", mensajeFinal, 
-                    presentarReporte(nombre, apellido, tipoNotas, 
-                    promedioEstudiante, numeroNotasArribaPromedio));
+            String notasMN = funcionNotasMN(filaNotas);
+            mensajeFinal = String.format("%s%s\n", mensajeFinal,
+                    presentarReporte(nombre, apellido, tipoNotas,
+                            promedioEstudiante, numeroNotasArribaPromedio, username, notasMN));
+
         }
+
         CrearArchivoTexto.agregarRegistros(mensajeFinal);
 
     }
-    
-    public static String presentarReporte(String nom, String ap, String notas, 
-            double prom, int numeroNotas){
+
+    public static String funcionNotasMN(int[] notas) {
+        String cadena;
+        int baja = notas[0];
+        int alta = notas[0];
+        for (int i = 0; i < notas.length; i++) {
+            if (alta < notas[1]) {
+                alta = notas[1];
+            }
+
+        }
+        for (int i = 0; i < notas.length; i++) {
+            if (baja < notas[1]) {
+                baja = notas[1];
+            }
+        }
+        cadena = String.format("Nota mas alta : %d\nNota mas baja : %d\n", alta, baja);
+
+        return cadena;
+    }
+
+    public static String funcionUser(String nombre, String apellido) {
+        String cadena;
+        String Inombre = nombre.substring(0, 1);
+
+        cadena = Inombre + "." + apellido + "@utpl.edu.ec";
+
+        return cadena;
+    }
+
+    public static String presentarReporte(String nom, String ap, String notas,
+            double prom, int numeroNotas, String username, String notasMN) {
         String reporte = String.format("Nombres: %s\n"
                 + "Apellidos: %s\n"
+                + "Username : %s\n"
                 + "Con notas: \n"
                 + "%s\n"
                 + "Promedio - %2f\n"
-                + "Número de notas arriba del promedio: %d\n\n",
-                nom, ap, notas, prom, numeroNotas);
-        
+                + "Número de notas arriba del promedio: %d\n\n"
+                + "%s\n",
+                nom, ap, username, notas, prom, numeroNotas, notasMN);
+
         return reporte;
     }
 
-    
-    public static double obtenerPromedioParalelo(int [][] n){
+    public static double obtenerPromedioParalelo(int[][] n) {
         int suma = 0;
         double promedio;
         int contador = 0;
@@ -64,12 +100,12 @@ public class Principal1 {
                 contador = contador + 1;
             }
         }
-        
-        promedio = (double)suma/ contador;
+
+        promedio = (double) suma / contador;
         return promedio;
     }
-    
-    public static double funcion01(int [] notas){
+
+    public static double funcion01(int[] notas) {
         int suma = 0;
         double promedio;
         for (int i = 0; i < notas.length; i++) {
@@ -78,9 +114,9 @@ public class Principal1 {
         promedio = (double) suma / notas.length;
         return promedio;
     }
-    
-    public static int funcion02(int [] notas, double promedio){
-        
+
+    public static int funcion02(int[] notas, double promedio) {
+
         int contador = 0;
         int nota;
         for (int i = 0; i < notas.length; i++) {
@@ -88,36 +124,33 @@ public class Principal1 {
             if (nota > promedio) {
                 contador = contador + 1;
             }
-            
+
         }
-        
+
         return contador;
     }
-    
-    public static String funcion03(int [] notas){
+
+    public static String funcion03(int[] notas) {
         String cadena = "";
-        
+
         int nota;
         for (int i = 0; i < notas.length; i++) {
             nota = notas[i];
-            if (nota >=0 && nota<=20) {
+            if (nota >= 0 && nota <= 20) {
                 cadena = String.format("%s%d-%s\n", cadena, nota, "M");
-            }else{
-                if (nota >20 && nota<=50) {
+            } else {
+                if (nota > 20 && nota <= 50) {
                     cadena = String.format("%s%d-%s\n", cadena, nota, "MB");
-                }else{
+                } else {
                     if (nota > 50) {
                         cadena = String.format("%s%d-%s\n", cadena, nota, "S");
                     }
                 }
             }
-            
+
         }
-        
+
         return cadena;
     }
-    
-    
-    
-    
+
 }
